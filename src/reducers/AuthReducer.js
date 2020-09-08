7aeca208de0412303aab701ee2f928dd4c1ce5aa
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-08 12:31:41 -0600

    login user failed function

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,15 +1,19 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload }
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case EMAIL_CHANGED:
