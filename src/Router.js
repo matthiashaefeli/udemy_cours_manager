a58a540a76e4dcfb4de1f7b63c38fa78d1bc2203
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-09 14:28:49 -0600

    add component employee list

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Stack, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Stack key="root">
-        <Scene key="login" component={LoginForm} title="Please Login" />
+      <Stack key='root'>
+        <Scene key='login' component={LoginForm} title='Please Login' initial />
+        <Scene key='employeeList' component={EmployeeList} title='Employees' />
       </Stack>
     </Router>
   );
