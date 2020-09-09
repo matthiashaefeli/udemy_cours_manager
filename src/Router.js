ce3b5557bc582fd9c6b54388e7b70eb33b9e714a
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-09 14:40:27 -0600

    nested navigation

--- src/Router.js
+++ src/Router.js
@@ -6,9 +6,13 @@ import EmployeeList from './components/EmployeeList';
 const RouterComponent = () => {
   return (
     <Router>
-      <Stack key='root'>
-        <Scene key='login' component={LoginForm} title='Please Login' initial />
-        <Scene key='employeeList' component={EmployeeList} title='Employees' />
+      <Stack key='root' hideNavBar>
+        <Scene key='auth'>
+          <Scene key='login' component={LoginForm} title='Please Login' initial />
+        </Scene>
+        <Scene key='main'>
+          <Scene key='employeeList' component={EmployeeList} title='Employees' />
+        </Scene>
       </Stack>
     </Router>
   );
