dae90f0daa8ba744ff98c6be1b211facac6cd7c5
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-10 09:09:49 -0600

    start form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label='Name'
+            placeholder='name'
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label='Phone'
+            placeholder='555-555-5555'
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+      </Card>
     );
   }
 }
