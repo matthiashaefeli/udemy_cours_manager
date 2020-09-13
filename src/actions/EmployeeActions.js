36923b0c44b317250750adca3f38805690675e8f
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-13 10:02:58 -0600

    submit data to firebas

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
   EMPLOYEE_UPDATE
 } from './types'
@@ -10,7 +11,8 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  return {
+  const { currentUser } = firebase.auth();
 
-  }
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 }
