a6ed2eab27607d97e7d4475afc299475028e8d54
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-23 09:05:30 -0600

    wiring up delete

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm'
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -25,6 +25,15 @@ class EmployeeEdit extends Component {
     const { phone, shift } = this.props;
 
     Communications.text(phone, `Your upcomming shift is on ${shift}`);
+  }
+
+  onDecline() {
+    this.setState({ showModal: false })
+  }
+
+  onAccept() {
+    const { uid } = this.props.employee
+    this.props.employeeDelete({ uid })
   }
 
   render() {
@@ -49,6 +58,8 @@ class EmployeeEdit extends Component {
 
         <Confirm
           visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this?
         </Confirm>
@@ -63,4 +74,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 }
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
