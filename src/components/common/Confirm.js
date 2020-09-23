a216bb1a788b4bf4043ca69352dfcfcd1604f9a4
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-23 08:38:22 -0600

    modal component without style

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,8 +3,26 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection, Button } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  return (
+    <Modal
+      animationType="slide"
+      onRequestClose={() => { }}
+      transparent
+      visible={visible}
+    >
+      <View>
+        <CardSection>
+          <Text>{children}</Text>
+        </CardSection>
 
-}
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
 
 export { Confirm };
